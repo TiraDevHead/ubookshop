1ff9421759888d0f8c0bc62c0123a3abb4ab0da9
TiraDevHead <$alizaraveshan@yahoo.com>
2024-08-30 02:34:27 +0330

    Update

---
+++ components/Pages/Block.tsx
@@ -0,0 +1,50 @@
+export const Block = props => {
+    if (!props.state.cart) {
+        props.state.cart = []
+      }
+    return <c-c style={{
+        width: 150, height: 290, minWidth: 150, margin: "10px 10px", position: "relative",flex:1,
+        backgroundColor: "white", boxShadow: "0 0 9px black",
+        borderRadius: 10,
+    }}
+        class={global.styles.hoverzoom_nofade_light}>
+        {<img
+            // className={global.styles.hover}
+            src={props.book.imagelink}
+            style={{
+                height: 200, width: "98%", objectFit: "fill",
+                 minWidth: 150, borderTopLeftRadius: 10,
+                borderTopRightRadius: 10,
+                margin:"1px 2px"
+            }}
+            onClick={() => {
+                props.state.form = "bookspecs"
+                props.state.book = props.book
+                props.refresh()
+            }} />}
+
+        <f-cc style={{ direction:"ltr",padding:"5px 8px" }}>
+            <f-12>{props.book.title}</f-12>
+        </f-cc>
+        <br-x/>
+        <br-x/>
+        <f-csb style={{ width: "100%", padding: "5px 5px" }}>
+
+            <div>
+                {props.state.cart.includes(props.book.title) ?
+                    <img src="https://irmapserver.ir/research/6/tik.png"
+                        style={{ height: 55, objectFit: "contain", position: "absolute", bottom: -2, right: -5 }} /> :
+                    <img src="https://irmapserver.ir/research/6/buy.png"
+                        style={{ height: 30, width: 30, objectFit: "contain", position: "absolute", bottom: 10, right: 10 }} />}
+            </div>
+
+            <c-x style={{ direction: "ltr", }}>
+                <f-12 style={{ color: "gray" }}><del>{((props.book.price) * 1.1 as number).toLocaleString("fa-IR")} تومان</del></f-12>
+                <f-14><b>{((props.book.price) as number).toLocaleString("fa-IR")} تومان</b></f-14>
+            </c-x>
+        </f-csb>
+    </c-c>
+}
+
+
+
--- components/Pages/index.tsx
+++ components/Pages/index.tsx
@@ -6,42 +6,133 @@ import TextBox from '@/components/Libs/TextBox';
 import Icon2Titles from '@/components/Libs/Icon2Titles';
 import Icon3Titles from '@/components/Libs/Icon3Titles';
 import css from '@/styles/css.module.css'
-// import '@/styles/css.module.css'
+import WindowFloat from '../Libs/WindowFloat';
+import './css.module.css';
+import { start } from 'repl';
+import { Block } from './Block';
 
 export default p => Component(p, Page);
 const Page: PageEl = (props, state, refresh, getProps) => {
 
   let styles = global.styles
-  let name = "خوش آمدید"
-
+  let total_price = 0
+  let benefit = 0
+  if (!state.cart) {
+    state.cart = []
+  }
+  for (let title of state.cart) {
+    let book = props.books.find(b => b.title == title)
+    if (book) {
+      total_price += (book.price)
+      benefit += (book.price * 0.1)
+    }
+  }
 
   return (
     <div style={{ direction: "rtl", minHeight: "11vh", }}>
       <br-x />
-      <Window title={name}
-        style={{
-          minHeight: 200, margin: 10, width: "calc(100% - 20px)",
-          backgroundImage: 'url("https://irmapserver.ir/research/0/bg.webp")',
-          // backgroundRepeat:"no-repeat",
-          backgroundSize: "cover",
-
-        }}>
+      <Window title={"سبد خرید"} style={{ minHeight: 80, margin: 10, width: "calc(100% - 20px)" }}>
         {/* <pre style={{ direction: "ltr" }}>{JSON.stringify(props, null, 2)}</pre> */}
-        <w-cse>
-          {props.books.map(book=>{
-            return <img src={book.imageLink} style={{height:200,width:100,objectFit:"fill",flex:1}} />
-          })}
+        <br />
+        <f-cse style={{}}>
+          <f-14>
+            قابل پرداخت :  <f-14 style={{ textDecoration: "underline" }}>{total_price.toLocaleString("fa-IR")}</f-14>  تومان
+          </f-14>
+          <f-14>
+            تعداد کتاب های موجود در سبد خرید : <f-14 style={{ textDecoration: "underline" }}>{state.cart.length.toLocaleString("fa-IR")}</f-14> عدد
+          </f-14>
+          <f-14>میزان سود شما از این خرید : <f-14 style={{ color: "forestgreen", textDecoration: "underline" }}>{benefit.toLocaleString("fa-IR")}</f-14> تومان</f-14>
+        </f-cse>
+      </Window>
 
-        </w-cse>
+      {state.form == "bookspecs" ? <WindowFloat title="مشخصات کتاب" onclose={() => {
+        delete state.form
+        refresh()
+      }}>
+        {/* <pre>{JSON.stringify(state.book,null,2)}</pre> */}
+        <f-c>
+          <f-15>نام کتاب:</f-15>
+          <sp-2 />
+          <f-15>{state.book.title}</f-15>
+        </f-c>
 
+        <f-c>
+          <f-15>نویسنده:</f-15>
+          <sp-2 />
+          <f-15>{state.book.author}</f-15>
+        </f-c>
 
+        <f-c>
+          <f-15>کشور:</f-15>
+          <sp-2 />
+          <f-15>{state.book.country}</f-15>
+        </f-c>
 
+        <f-c>
+          <f-15>زبان:</f-15>
+          <sp-2 />
+          <f-15>{state.book.language}</f-15>
+        </f-c>
+
+        <f-c>
+          <f-15>تعداد صفحات:</f-15>
+          <sp-2 />
+          <f-15>{state.book.pages.toLocaleString("fa-IR")} صفحه</f-15>
+        </f-c>
+
+        <f-c>
+          <f-15>سال انتشار:</f-15>
+          <sp-2 />
+          <f-15>{(state.book.year as number).toLocaleString("fa-IR")} میلادی</f-15>
+        </f-c>
+
+        <f-c>
+          <f-15>قیمت:</f-15>
+          <sp-2 />
+          <f-15>{state.book.price.toLocaleString("fa-IR")} تومان</f-15>
+        </f-c>
+
+        <f-x style={{ direction: "ltr" }}>
+          <g-b style={{
+            height: 30, margin: "5px 5px", maxWidth: 150,
+            backgroundColor: state.cart.includes(state.book.title) ?
+              "#FA3636D8" : "#29E454A6", color: "#080808",
+            borderRadius: "10px"
+          }}
+            onClick={() => {
+              if (state.cart.includes(state.book.title)) {
+                state.cart = state.cart.filter(bookname => state.book.title != bookname)
+                state.form = null
+                refresh()
+                alert(" کتاب" + " " + state.book.title + " " + " از سبد خرید شما حذف گردید")
+              }
+              else {
+                state.cart.push(state.book.title)
+                state.form = null
+                refresh()
+              }
+            }}>
+            {state.cart.includes(state.book.title) ?
+              <f-13>حذف از سبد خرید</f-13> : <f-13>افزودن به سبد خرید</f-13>
+            }
+          </g-b>
+        </f-x>
+      </WindowFloat> : null}
+      <Window title={"خوش آمدید"} style={{ minHeight: 200, margin: 10, width: "calc(100% - 20px)", gap: 10 }}>
+        <c-c style={{ width: "100%" }}>
+          <w-cse style={{}}>
+            {props.books.map(book => {
+              return <Block
+                book={book}
+                state={state}
+                refresh={refresh} />
+            })}
+          </w-cse>
+        </c-c>
       </Window>
     </div>
   )
 }
-
-
 export async function getServerSideProps(context) {
 
 
@@ -53,9 +144,9 @@ export async function getServerSideProps(context) {
 
   let books = await global.db.collection("books").find({}).toArray()
   for (let book of books) {
-    book.imageLink = "https://irmapserver.ir/research/ex/books/" + book.imageLink
-
+    book.imagelink = "https://irmapserver.ir/research/ex/books/" + book.imageLink
   }
+  console.log(books)
 
   return {
     props: {
